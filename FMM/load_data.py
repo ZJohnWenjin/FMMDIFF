a9d5cb69aa9d63a6fd5c1b643f7e9c9d598f151b
ZJohnWenjin <143795101+ZJohnWenjin@users.noreply.github.com>
2025-10-30 22:04:32 +0800

    Add files via upload

---
+++ FMM/load_data.py
@@ -0,0 +1,74 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import nibabel as nib
+
+
+def get_all_nii_paths(root_dir, m_list):
+    all_cases = []
+
+    for subdir in sorted(os.listdir(root_dir)):
+        subdir_path = os.path.join(root_dir, subdir)
+        if not os.path.isdir(subdir_path):
+            continue
+
+        case_paths = []
+        for modality in m_list:
+            modality_path = os.path.join(subdir_path, modality)
+            if os.path.exists(modality_path):
+                case_paths.append(modality_path)
+            else:
+                break
+        if len(case_paths) == len(m_list):
+            all_cases.append(case_paths)
+
+    return all_cases
+
+class NiiDataset(Dataset):
+    def __init__(self, file_list):
+        self.file_list = file_list
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        modalities_list = self.file_list[idx]
+        m_list_tensor = []
+        for i in range(len(modalities_list)):
+            labels_nii = nib.load(modalities_list[i])
+            m_numpy = self.min_max_normalize(labels_nii.get_fdata())
+            m_list_tensor.append(m_numpy)
+
+        m_list_tensor = torch.cat(m_list_tensor, dim=0)
+        # shape: (num_modality, H, W, Z）
+        return m_list_tensor
+
+    def min_max_normalize(self,arr):
+
+        min_val = np.min(arr)
+        max_val = np.max(arr)
+
+        normalized_arr = (arr - min_val) / (max_val - min_val)
+
+        if max_val == 0:
+          return np.zeros_like(arr)
+
+        normalized_arr = np.transpose(normalized_arr, (2, 0, 1))
+        tensor_nii = torch.from_numpy(normalized_arr).float().unsqueeze(1)  # shape: (Z, 1, W, H)
+
+        tensor_nii = F.interpolate(tensor_nii, size=(224, 224), mode='bilinear', align_corners=False)
+
+        return tensor_nii.permute(1,0,2,3) # shape: (1, Z, W, H)
+
+def build_loader_for_FMM(data_path,m_list):
+    nii_files = get_all_nii_paths(data_path,m_list)
+
+
+    dataset = NiiDataset(nii_files[:200])
+
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=12,
+                            pin_memory=True, drop_last=False)
+
+    return dataloader
---
+++ FMM/main.py
@@ -0,0 +1,112 @@
+import argparse
+import traceback
+import shutil
+import logging
+import yaml
+import sys
+import os
+import torch
+import numpy as np
+
+from models.FMM_pretrain import FMM_Pretrain
+
+torch.set_printoptions(sci_mode=False)
+
+
+def parse_args_and_config():
+    parser = argparse.ArgumentParser(description=globals()["__doc__"])
+
+    parser.add_argument(
+        "--config", type=str, default='config.yml', help="Path to the config file"
+    )
+    parser.add_argument(
+        "--train_specific_en_decoder",
+        default=True,
+        action="store",
+        help="Whether to train specific encoder and decoder",
+    )
+    parser.add_argument(
+        "--train_mapping_encoder",
+        default=True,
+        action="store",
+        help="Whether to train mapping encoder",
+    )
+    parser.add_argument(
+        "--log_path",
+        type=str,
+        default="log",
+        action="store",
+    )
+    args = parser.parse_args()
+
+    # parse config file
+    with open(args.config, "r", encoding="utf-8") as f:
+        cfg_obj = yaml.safe_load(f)
+
+    if not isinstance(cfg_obj, dict):
+        raise ValueError(f"YAML root must be a mapping (dict), got {type(cfg_obj).__name__}")
+
+    new_config = dict2namespace(cfg_obj)
+
+    if os.path.exists(args.log_path):
+        overwrite = False
+        response = input("Folder already exists. Overwrite? (Y/N)")
+        if response.upper() == "Y":
+            overwrite = True
+
+        if overwrite:
+            shutil.rmtree(args.log_path)
+            os.makedirs(args.log_path)
+    else:
+        os.makedirs(args.log_path)
+
+    handler1 = logging.StreamHandler()
+    handler2 = logging.FileHandler(os.path.join(args.log_path, "stdout.txt"))
+    formatter = logging.Formatter(
+        "%(levelname)s - %(filename)s - %(asctime)s - %(message)s"
+    )
+    handler1.setFormatter(formatter)
+    handler2.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    logger.setLevel(20)
+
+    # add device
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    logging.info("Using device: {}".format(device))
+
+    return args, new_config
+
+
+def dict2namespace(config):
+    if isinstance(config, dict):
+        ns = argparse.Namespace()
+        for k, v in config.items():
+            setattr(ns, k, dict2namespace(v))
+        return ns
+    elif isinstance(config, list):
+        return [dict2namespace(v) for v in config]
+    else:
+        # 标量类型（str/int/float/bool/None）原样返回
+        return config
+
+def main():
+    args, config = parse_args_and_config()
+    logging.info("Writing log file to {}".format(args.log_path))
+    logging.info("Exp instance id = {}".format(os.getpid()))
+
+
+    if args.train_specific_en_decoder or args.train_mapping_encoder:
+        FMM_P = FMM_Pretrain(config)
+        if args.train_specific_en_decoder:
+            FMM_P.pretrain(specific_encoder=True)
+        if args.train_mapping_encoder:
+            FMM_P.pretrain(specific_encoder=False)
+
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
